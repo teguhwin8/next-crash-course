ebfb1f88c9eab582621b6a61cb37f20f3f2f55a0
Teguh Widodo <teguhwin8@gmail.com>
2021-04-28 22:25:03 +0700

    Update server config

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://mystifying-hodgkin-fef6c8.netlify.app'
+export const server = dev ? 'http://localhost:3000' : 'https://jsonplaceholder.typicode.com/'
